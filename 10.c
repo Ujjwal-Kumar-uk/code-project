82c597fb5caac9a83eb296a94648960575054dfb
Ujjwal Kumar <ujjwalkumar4590@gmail.com>
2025-09-07 20:08:44 +0530

    Delete 10.c

--- 10.c
+++
@@ -1,9 +0,0 @@
-//WRITE YOUR OWN VERSION OF STRCPY() FUNCTION FROM STRING.H
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char c[] = "ujjwal";
-    int a = strlen(c);
-    printf("%d",a);
-    return 0; 
-}
